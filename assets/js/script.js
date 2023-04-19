d5f68d69abfd6c6272fa792db078310255b3052f
Samuel Carmo <samuelcarmo_2@hotmail.com>
2023-04-19 20:40:36 +0100

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+const buttons = document.querySelectorAll("button")
+const calc = document.getElementById("calc")
+const screenResult = document.getElementById("result")
+
+var a, b, op, result
+
+// IT'S MISSING ROUND THE RESULT LIKE "result.toFixed(10)"
+
+const buttonPressed = e => {
+    calcule(e.target.value)
+}
+
+for (let button of buttons) {
+    button.addEventListener("click", buttonPressed)
+}
+
+function calcule(value) {
+    if (value != "=" && value != "CE" && value != "square" && value != "%") {
+        result = null
+        if (op == null && (!isNaN(value) || value == ".")) {
+            if (a == null) {
+                a = value
+            } else {
+                a += value
+            }
+        } else if (a != null && op != null && (!isNaN(value) || value == ".")) {
+            if (b == null) {
+                b = value
+            } else {
+                b += value
+            }
+        } else if (a != null && isNaN(value)) {
+            op = value
+        }
+        calc.innerText += value
+    } else if (value === "=") {
+        if (op === "+") {
+            result = +a + +b
+        } else if (op === "-") {
+            result = +a - +b
+        } else if (op === "/") {
+            result = +a / +b
+        } else if (op === "x") {
+            result = +a * +b
+        } else if (op === "%") {
+            result = (+a * +b) / 100
+        }
+        screenResult.innerText = result
+    } else if (value === "CE") {
+        a = null
+        b = null
+        op = null
+        result = null
+        calc.innerText = ""
+        screenResult.innerText = ""
+    } else if (value === "square") {
+        result = Math.sqrt(a, 2)
+        screenResult.innerText = result
+    } else if (value === "%") {
+        if (op == null) {
+            op = "%"
+        } else if (op === "x") {
+            result = (+a * +b) / 100
+        } else if (op === "-") {
+            result = ((100 - +b) * +a) / 100
+        } else if (op === "+") {
+            result = +a * (100 + +b) / 100
+        }
+        calc.innerText += value
+        screenResult.innerText = result
+    }
+}
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/css.css" />
+    <title>Calculator</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- <div class="title"> -->
+            <h1>Calculator</h1>
+        <!-- </div> -->
+        <div class="calculator">
+            <div class="visor">
+                <div id="calc">
+
+                </div>
+                <div id="result">
+                </div>
+            </div>
+            <div>
+                <button value="CE">CE</button>
+                <button value="square">&#8730</button>
+                <button value="%">%</button>
+                <button value="/">&#247</button>
+            </div>
+            <div>
+                <button value="7">7</button>
+                <button value="8">8</button>
+                <button value="9">9</button>
+                <button value="x">x</button>
+            </div>
+            <div>
+                <button value="4">4</button>
+                <button value="5">5</button>
+                <button value="6">6</button>
+                <button value="-">-</button>
+            </div>
+            <div>
+                <button value="1">1</button>
+                <button value="2">2</button>
+                <button value="3">3</button>
+                <button value="+">+</button>
+            </div>
+            <div>
+                <button value="0">0</button>
+                <button value=".">.</button>
+                <button class="equals" value="=">=</button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<script src="assets/js/script.js"></script>
